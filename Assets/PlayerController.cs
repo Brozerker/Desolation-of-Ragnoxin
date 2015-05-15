c015df9ac6b052d358e54bdd8615dc32abf19982
Brozerker <chris.randall619@yahoo.com>
2015-05-15 13:58:10 -0700

    Basic first level

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public int speed;
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+    }
+}
