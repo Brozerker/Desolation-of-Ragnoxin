793319ad2ae5391874adee894202fd64c4dc2d25
Brozerker <chris.randall619@yahoo.com>
2015-06-02 09:27:20 -0700

    updated goal sprite

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -5,7 +5,8 @@ public class GoalController : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
             Debug.Log("entered");
-            Application.LoadLevel("Level2");
+            //Application.LoadLevel("Level2");
+            Application.LoadLevel(Application.loadedLevel+1);
         }
 	
 	}
