24ab0831c7aa7cc6a10a09d53146f6d24004e9c6
Brozerker <chris.randall619@yahoo.com>
2015-06-04 15:50:15 -0700

    sorta enemy ai

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class enemyController : MonoBehaviour {
+    public float distance;
+    public float speed;
+    
+    private Vector3 startPos;
+    private Animator animator;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    void Start() {
+        animator = GetComponent<Animator>();
+        startPos = transform.position;
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        if (transform.position.x < startPos.x || transform.position.x > (startPos.x + distance)) {
+            animator.SetBool("moveLeft", !(animator.GetBool("moveLeft")));
+            speed *= -1;
+        }
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, 0.0f);
 	}
 }
