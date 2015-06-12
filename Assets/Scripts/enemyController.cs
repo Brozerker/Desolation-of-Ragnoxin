4600d50b570847b550dd2270a96782cd9da79380
Brozerker <chris.randall619@yahoo.com>
2015-06-12 13:22:16 -0700

    more scripting

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -5,7 +5,7 @@ public class enemyController : MonoBehaviour {
     public float distance;
     public float speed;
     public float attackPower;
-    public float waitTime;
+    public float attackDelay = 0.7f;
     //private float startTimer;
     private Vector3 startPos;
     private Animator animator;
@@ -18,26 +18,33 @@ public class enemyController : MonoBehaviour {
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
-        waitTime = 0.7f;
         animator = GetComponent<Animator>();
         facingLeft = true;
         startPos = transform.position;
+        attacking = false;
     }
 
     // Update is called once per frame
     void Update() {
         if (attacking) {
+            Attack();
             //startTimer = 0;
-            if ((Time.deltaTime % waitTime) < 1) {
-                Debug.Log("attack");
-            }
+            //if ((Time.deltaTime % waitTime) < 1) {
+            //}
         }
         else {
-            if (Mathf.Abs(transform.position.x - player.transform.position.x) < 1) {
+            if (Mathf.Abs(transform.position.x - player.transform.position.x) < 1)  {
                 useCase = UseCase.seek;
             }
             updateMovement();
         }
+    }
+
+    IEnumerator Attack() {
+        animator.SetTrigger("attack");
+        Debug.Log("attack");
+        // player health -= attackPower;
+        yield return new WaitForSeconds(attackDelay);
     }
 
     void updateMovement() {
