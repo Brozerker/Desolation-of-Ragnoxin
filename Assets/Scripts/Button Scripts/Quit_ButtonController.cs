4e25a3525627f92a1e732ed4524228757c844e5d
Flamarow <Flamarow@hotmail.com>
2015-06-21 05:01:16 -0400

    General level fixes
    Fixed a few rendering bugs in the levels , fixed a compiler problem with
    UnityEditor, and made levels easier to understand.

--- Assets/Scripts/Button Scripts/Quit_ButtonController.cs
+++ Assets/Scripts/Button Scripts/Quit_ButtonController.cs
@@ -7,8 +7,13 @@ public class Quit_ButtonController : ButtonController {
 	{
 		source.PlayOneShot (pressed);
 		Debug.Log ("Starting Level 1");
-		Application.Quit();
+
+#if UNITY_EDITOR
 		// Application.Quit doesn't work in the editor, so the next line closes the game there instead
+		// Now allows the game to be built while maintaning editor commands
 		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
 	}
 }
